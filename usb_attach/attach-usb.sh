3f47c124362de726f7899d015eb8a76feca5a6bf
sdtimothy8 <sdtimothy8@gmail.com>
2016-12-28 13:43:33 +0800

    one script to auto attach/detach the usb from real machine to virtual machine

---
+++ usb_attach/attach-usb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if (( $# != 1 )); then
+	echo 'error:input vm id'
+	exit 2
+fi
+
+vendorId=`lsusb | tail -n1 | awk '{print $6}' | awk -F: '{print $1}'`
+productId=`lsusb | tail -n1 | awk '{print $6}' | awk -F: '{print $2}'`
+
+echo "<hostdev mode='subsystem' type='usb'>
+        <source>
+                <vendor id='0x${vendorId}'/>
+                <product id='0x${productId}'/>
+        </source>
+</hostdev>" > usb.xml
+
+virsh attach-device $1 usb.xml
